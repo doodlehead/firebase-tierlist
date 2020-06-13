189f3fa1aa7b8c7fdc50f7937063eb8b7899aa5f
Frank <frankyu1999@gmail.com>
2020-06-13 02:18:18 -0400

    Add support for CORS,  add error responses, format time response

--- functions/index.js
+++ functions/index.js
@@ -5,28 +5,41 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 const db = admin.firestore();
 const axios = require('axios').default;
+const cors = require('cors')({
+  origin: true,
+});
 
 const EXPIRY_PERIOD = 43200000; //12 hours
 
+/**
+ * Fetches the token from TBDB's /login endpoint, stores it in the db,
+ * and then fires the response.
+ * @param {Function} response - the https response callback method
+ * @returns {string} the token's value
+ */
 const saveToken = response => {
   console.log('fetching new auth token from TVDB');
   axios.post('https://api.thetvdb.com/login', {
     apiKey: process.env.TVDB_API_KEY
   })
   .then(res => {
-    //console.log(res);
     const data = {
       value: res.data.token,
       createdAt: new Date()
     }
 
-    db.collection('tokens').doc('api-request-token').set(data);
-    //db.collection('tokens').add(data);
-    if (response) response.send(data);
+    db.collection('tokens')
+      .doc('api-request-token')
+      .set(data);
 
-    return res;
+    response?.send(data);
 
-  }).catch(err => console.error(err));
+    return data.value;
+
+  }).catch(err => {
+    console.error(err);
+    response.status(500).send('Cannot get token, request to API failed');
+  });
 }
 
 /**
@@ -34,34 +47,38 @@ const saveToken = response => {
  * - use async/await for fetch
  * - helper method for date comparison
  * - better constants?
- * - send cleaner data? Format Timestamp properly (timeMillis?)
  */
 exports.getToken = functions.https.onRequest(async (request, response) => {
-  const tokenRef = db.collection('tokens').doc('api-request-token');
+  return cors(request, response, () => {
+    const tokenRef = db.collection('tokens').doc('api-request-token');
   
-  tokenRef.get().then(doc => {
-    if (!doc.exists) {
-      saveToken(response);
-    } else {
-      const data = doc.data();
-
-      if (new Date().valueOf() - data.createdAt.toMillis() > EXPIRY_PERIOD) {
-        saveToken(response); //update token if 12 hours have passed
+    tokenRef.get().then(doc => {
+      if (!doc.exists) {
+        saveToken(response);
       } else {
-        response.send(doc.data());
+        let data = doc.data();
+  
+        if (new Date().valueOf() - data.createdAt.toMillis() > EXPIRY_PERIOD) {
+          saveToken(response); //update token if 12 hours have passed
+        } else {
+          data.createdAt = data.createdAt.toMillis()
+          response.send(data);
+        }
       }
-    }
-    return doc;
-  }).catch(err => console.log(err))
-})
+      return doc;
+    }).catch(err => {
+      console.error(err);
+      response.status(500).send('Cannot get token');
+    })
+  });
+});
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
- //saveToken();
 });
 
-exports.updateToken = functions.pubsub.schedule('every 30 seconds')
-  .onRun(async context => {
-    console.log(context);
-    saveToken();
-  });
+// exports.updateToken = functions.pubsub.schedule('every 30 seconds')
+//   .onRun(async context => {
+//     console.log(context);
+//     saveToken();
+//   });
