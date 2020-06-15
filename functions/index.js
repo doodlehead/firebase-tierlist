de7d1c8669e8ab54f6553efdfccb0cec261637d6
Frank <frankyu1999@gmail.com>
2020-06-14 21:48:57 -0400

    Use async/await so return works

--- functions/index.js
+++ functions/index.js
@@ -17,14 +17,15 @@ const EXPIRY_PERIOD = 43200000; //12 hours
  * @param {Function} response - the https response callback method
  * @returns {string} the token's value
  */
-const saveToken = response => {
+const saveToken = async response => {
   console.log('fetching new auth token from TVDB');
-  axios.post('https://api.thetvdb.com/login', {
+  const apiRes = await axios.post('https://api.thetvdb.com/login', {
     apiKey: process.env.TVDB_API_KEY
-  })
-  .then(res => {
+  });
+
+  if (apiRes) {
     const data = {
-      value: res.data.token,
+      value: apiRes.data.token,
       createdAt: new Date()
     }
 
@@ -35,11 +36,10 @@ const saveToken = response => {
     response?.send(data);
 
     return data.value;
-
-  }).catch(err => {
+  } else {
     console.error(err);
     response?.status(500).send('Cannot get token, request to API failed');
-  });
+  }
 }
 
 /**
@@ -68,38 +68,6 @@ const getToken = async () => {
   }
 }
 
-
-/**
- * TODO:
- * - use async/await for fetch
- * - helper method for date comparison
- * - better constants?
- */
-// exports.getToken = functions.https.onRequest(async (request, response) => {
-//   return cors(request, response, () => {
-//     const tokenRef = db.collection('tokens').doc('api-request-token');
-  
-//     tokenRef.get().then(doc => {
-//       if (!doc.exists) {
-//         saveToken(response);
-//       } else {
-//         let data = doc.data();
-  
-//         if (new Date().valueOf() - data.createdAt.toMillis() > EXPIRY_PERIOD) {
-//           saveToken(response); //update token if 12 hours have passed
-//         } else {
-//           data.createdAt = data.createdAt.toMillis()
-//           response.send(data);
-//         }
-//       }
-//       return doc;
-//     }).catch(err => {
-//       console.error(err);
-//       response.status(500).send('Cannot get token');
-//     })
-//   });
-// });
-
 /**
  * The TVDB's API doesn't support CORS, so this function will serve as a proxy
  * to call their API. Only works for GET requests for now.
@@ -121,14 +89,6 @@ exports.proxy = functions.https.onRequest(async (req, res) => {
     }
   });
 });
-
-// exports.test = functions.https.onRequest(async (req, res) => {
-//   const token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTI2OTQ5NzUsImlkIjoiVGllcmxpc3QiLCJvcmlnX2lhdCI6MTU5MjA5MDE3NX0.ikQPS5a5bY4ASNV-oK3u2bjwd5XotWHiI3zWhg98hn6HMuHtlc3YSB5qOK6FWR2XP8XjIyimaHXYI1_p8cZrxktVjlyR5I8ERRIPYviA1pMLpZ4yVusL869vkrYJGlDn3NKOZCwDEhKpECaObBo-DHiFLoyhoKwkDhDTx2SMM79EWEVOPuOpW0wTE7NiXxPmG0uA2kWdqxb74tHC9eZjrRIREHIHtRuWlhtIxQoSBgVKgn8Rx4BeWz-AtdaKbIMaWi0dtpoIIkJuGvcXYwmtw-VqMs2Vbo3PFlv2_IY-F1qf0SE6YuNHaZ4OvblBxzIwVXZojyoU2qRMTb5iXci1Fw'
-//   const apiRes = await axios.get('https://api.thetvdb.com/search/series?name=game', {
-//     headers: { Authorization: `Bearer ${token}`}
-//   });
-//   res.send(apiRes.data);
-// })
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
